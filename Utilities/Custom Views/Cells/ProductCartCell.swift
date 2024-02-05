0c72d6f62e4cf71dd7d0bab00f63c3dd10f74915
Ahmed Mohammed Yamany <yamany2862016@gmail.com>
2024-02-05 22:09:57 +0200

    product cart cell

--- Utilities/Custom Views/Cells/ProductCartCell.swift
+++ Utilities/Custom Views/Cells/ProductCartCell.swift
@@ -5,4 +5,39 @@
 //  Created by Ahmed Yamany on 05/02/2024.
 //
 
-import Foundation
+import SwiftUI
+
+struct ProductCartCell: View {
+    let product: Product
+    let count: Int
+    
+    @State var value = 1
+    
+    var body: some View {
+        HStack(alignment: .top, spacing: .Constants.cellSpacing) {
+            Image(uiImage: product.firstImage())
+                .resizable()
+                .aspectRatio(contentMode: .fill)
+                .frame(width: 82, height: 100)
+                .clipShape(RoundedRectangle(cornerRadius: .Constants.cornerRadius))
+            
+            VStack(alignment: .leading, spacing: .Constants.cellPadding) {
+                ProductCellContent(product: product)
+                
+                stepperView
+            }
+        }
+        .frame(maxWidth: .infinity, alignment: .leading)
+        .padding(.Constants.cellPadding)
+        .background(.grInputField)
+        .clipShape(RoundedRectangle(cornerRadius: .Constants.cornerRadius))
+        .onAppear {
+            value = count
+        }
+    }
+    
+    var stepperView: some View {
+        Text("\(value)")
+    }
+    
+}
